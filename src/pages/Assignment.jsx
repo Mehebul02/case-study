479908cbd1f2f795913064e98fa95c73f071c750
Mehebul02 <mehebul2123@gmail.com>
2024-05-10 12:36:16 +0600

    assignment successfully

--- src/pages/Assignment.jsx
+++ src/pages/Assignment.jsx
@@ -1,26 +1,54 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import AssignmentCard from "./AssignmentCard";
+// import { IoIosArrowDown } from "react-icons/io";
 
 const Assignment = () => {
  const [assignments ,setAssignments] = useState([])
-
+const [filter , setFilter]= useState('')
   useEffect(() => {
     getData();
-  }, []);
+  }, [filter]);
 
   const getData = async () => {
-    const { data } = await axios(`${import.meta.env.VITE_API_URL}/assignments`);
+    const { data } = await axios(`${import.meta.env.VITE_API_URL}/assignments?filter =${filter}`);
     // console.log(data);
     setAssignments(data)
   };
   console.log(assignments)
   return (
+    <div>
+      <div className="max-w-[1300px] mx-auto text-center">
+      {/* <div className="dropdown dropdown-end ">
+  <div tabIndex={0} role="button" className="bg-green-700 px-6 m-1 flex items-center gap-3 text-white py-2 font-serif text-xl font-semibold mt-7 rounded-lg">Filter <IoIosArrowDown/></div>
+  <ul tabIndex={0} className="dropdown-content  z-[1] menu p-2 text-xl font-medium font-serif shadow bg-base-200 rounded-box w-52">
+    <li ><a>easy</a></li>
+    <li><a>medium</a></li>
+    <li><a>hard</a></li>
+  </ul>
+</div> */}
+<select
+              onChange={e => {
+                setFilter(e.target.value)
+                
+              }}
+              value={filter}
+              name='difficulty'
+              id='difficulty'
+              className='border p-4 rounded-lg'
+            >
+              <option value=''>Filter By Category</option>
+              <option value='easy'>easy</option>
+              <option value='medium'>medium</option>
+              <option value='hard'>hard</option>
+            </select>
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
       {
         assignments.map(assignment => <AssignmentCard key={assignment._id} assignment={assignment}></AssignmentCard>)
       }
       
+    </div>
+    </div>
     </div>
   );
 };
--- src/pages/AssignmentCard.jsx
+++ src/pages/AssignmentCard.jsx
@@ -1,37 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
-const AssignmentCard = ({assignment}) => {
-    const {_id, thumbnail, title, mark, difficulty } = assignment;
+const AssignmentCard = ({ assignment }) => {
+  const { user } = useAuth();
+  const { _id, email, thumbnail, title, mark, difficulty } = assignment;
 
-    // const handleUpdate =id=>{
-    //     console.log(id)
-    // }
-    return (
-        <div className="bg-white rounded-lg shadow-md p-4">
-        <img
-          src={thumbnail}
-          className=""
-        />
-        <h3 className="text-lg font-poppins font-semibold">{title}</h3>
-        <p className="text-gray-600 mb-2">Marks: {mark}</p>
-        <p className="text-gray-600 mb-2">Difficulty: {difficulty}</p>
-        <div className="flex justify-between mt-4">
-          <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
-            Delete
-          </button>
-         <Link to={`/update/${_id}`}>
-         <button  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+  const handleDelete = async (id) => {
+    try {
+      if (user.email !== email) {
+        toast.error("You can only delete assignments created by you.");
+        return;
+      }
+      const { data } = await axios.delete(
+        `${import.meta.env.VITE_API_URL}/assignments/${id}`
+      );
+      console.log(data);
+      toast.success("Assignment delete successfully");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div className="bg-white rounded-lg shadow-md p-4">
+      <img src={thumbnail} className="" />
+      <h3 className="text-lg font-poppins font-semibold">{title}</h3>
+      <p className="text-gray-600 mb-2">Marks: {mark}</p>
+      <p className="text-gray-600 mb-2">Difficulty: {difficulty}</p>
+      <div className="flex justify-between mt-4">
+        <button
+          onClick={() => handleDelete(_id)}
+          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+        >
+          Delete
+        </button>
+        <Link to={`/update/${_id}`}>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
             Update
           </button>
-         </Link>
+        </Link>
 
-          <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
-            View
-          </button>
-        </div>
+        <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
+          View
+        </button>
       </div>
-    );
+    </div>
+  );
 };
 
 export default AssignmentCard;
